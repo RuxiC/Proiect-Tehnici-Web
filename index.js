ee639a83dd8bab8b7b830401a508e93a8c4f430a
RuxiC <ruxandra.cirstea@gmail.com>
2023-04-26 20:09:42 +0300

    added proiect

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
 const sass = require("sass");
+/*const {Client} = require("pg");*/
+
+/*
+var client= new Client({database:"tehnici_web",
+        user:"ruxandra",
+        password:"1234",
+        host:"localhost",
+        port:5432});
+client.connect();
+
+client.query("select * from lab8_10", function(err, rez){
+    console.log("eroare:", err);
+    console.log("rezultat:", rez);
+});
+*/
 
 // express ne-a ajutat sa cream un server
 app = express();  // etapa 4 - ex 2
@@ -10,30 +25,78 @@ app = express();  // etapa 4 - ex 2
 obGlobal={
     obErori:null,
     obImagini:null,
-    folderScss: path.join(__dirname, "resurse/scss"),  // etapa 6 - a)
-    folderCss: path.join(__dirname, "resurse/css")
+    folderScss: path.join(__dirname, "resurse/scss"),  // etapa 5 - a)
+    folderCss: path.join(__dirname, "resurse/css"),
+    folderBackup: path.join(__dirname, "backup")
 }
+/*
+client.query("select * from unnest(enum_range(null::categ_prajitura)))",function(err, rezTipuri){
+    if(err){
+        console.log(err)
+    }
+    else{
+        obGlobal.optiuniMeniu-rezTipuri.rows
+    }
+
+})
+*/
 
 // etapa 4 - ex3
 console.log("Folder proiect", __dirname); 
 console.log("Cale fisier", __filename);
 console.log("Director de lucru", process.cwd());
 
-// etapa 4 - ex 20
+// etapa 4 - ex 20 si etapa 5 c)
 vectorFoldere=["temp", "temp1", "resurse", "backup"]  // am creat temp si temp 1
 for(let folder of vectorFoldere){
    // let caleFolder=__dirname+"/"+folder
-    let caleFolder=path.join(__dirname,folder);
-    if(!fs.existsSync(caleFolder))
-        fs.mkdirSync(caleFolder);
+   let caleFolder=path.join(__dirname, folder)
+   if (! fs.existsSync(caleFolder)){ //nu (exista in mod sincron, nu trebuie callback)
+       fs.mkdirSync(caleFolder); //creaza director in mod sincron
+   }
 }
 
 
+//etapa 5 b
+/*sa nu compilam manual de fiecare data scss*/
 function compileazaScss(caleScss, caleCss){
-    if(!path.isAbsolute(caleScss))
-        caleCss=path.join(obGlobal.folderScss.caleScss);
+    console.log("cale:",caleCss);
+    if(!caleCss){ /*undifind*/ /*lista cu fi+olderele din cale*/
+        let vectorCale=caleScss.split("\\")
+        let numeFisExt=vectorCale[vectorCale.length-1]; /*cu extensie*/
 
+        let numeFis=numeFisExt.split(".")[0]   /// "a.scss"  -> ["a","scss"]
+        caleCss=numeFis+".css";
+    }
+    
+    if (!path.isAbsolute(caleScss)) //nu e cale absoluta, pun fisierele chiar in folderele absolute; /,#
+        caleScss=path.join(obGlobal.folderScss, caleScss)
+    if (!path.isAbsolute(caleCss))
+        caleCss=path.join(obGlobal.folderCss, caleCss)
+    
+    // la acest punct avem cai absolute in caleScss si  caleCss
+    let vectorCale=caleCss.split("\\");
+    let numeFisCss=vectorCale[vectorCale.length-1]; 
+    if (fs.existsSync(caleCss)){ /* etapa 5 d)  copiere in backup daca exista fisierul css*/
+        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, numeFisCss))// +(new Date()).getTime()
+    }
+    /*ob cu prop css, e string rezultat din cod css*/
+    rez=sass.compile(caleScss, {"sourceMap":true}); /*vad linia din fisierul sass, chiar daca browserul nu primeste fisierul sass*/
+    fs.writeFileSync(caleCss,rez.css)
+    console.log("Compilare SCSS",rez);
 }
+//compileazaScss("a.scss");
+//etapa 5 e)
+/*verifica daca au suvernit schimbari; din scss sa l fac css*/
+fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
+    console.log(eveniment, numeFis);
+    if (eveniment=="change" || eveniment=="rename"){
+        let caleCompleta=path.join(obGlobal.folderScss, numeFis);
+        if (fs.existsSync(caleCompleta)){
+            compileazaScss(caleCompleta);
+        }
+    }
+})
 
 app.set("view engine", "ejs"); // in ejs scriem template-uri
 
@@ -62,8 +125,53 @@ app.get("/ceva", function(req, res){
 
 // etapa 4 - ex8 si ex 16
 app.get(["/index", "/", "/home"], function(req, res){
-    res.render("pagini/index", {ip: req.ip, a:10, b:20}); // render-sa compileze/afiseze, transforma include-ul din index.ejs
+    res.render("pagini/index", {ip: req.ip, a:10, b:20, imagini:obGlobal.obImagini.imagini}); // render-sa compileze/afiseze, transforma include-ul din index.ejs in html
 });                                                       // proprietatile lui locals in req.ip
+
+
+
+/*
+app.get("/produse",function(req, res){
+    console.log(req.query);
+
+    //TO DO query pentru a selecta toate produsele
+    //TO DO se adauaga filtrarea dupa tipul produsului
+    //TO DO se selecteaza si toate valorile din enum-ul categ_prajitura
+
+    client.query("select * from unnest(enum_range(null::categ_prajitura))",function(err, rez){
+     //   console.log(err);
+     //   console.log(rez);
+    })
+        let conditieWhere=""
+        if(req.query.tip)
+            conditieWhere= `where tip_produs= '${req.query.tip}'`;
+        client.query("select * from prajituri"+conditieWhere , function( err, rez){
+            console.log(300)
+            if(err){
+                console.log(err);
+                renderError(res, 2);
+            }
+            else
+                res.render("pagini/produse", {produse:rez.rows, optiuni:rezCategorie.rows});
+        });
+
+
+});
+
+
+app.get("/produs/:id",function(req, res){
+    console.log(req.params);
+   
+    client.query(" TO DO ", function( err, rezultat){
+        if(err){
+            console.log(err);
+            afisareEroare(res, 2);
+        }
+        else
+            res.render("pagini/produs", {prod:""});
+    });
+});
+*/
 
 // etapa 4 - ex 19
 //app.get("/\.ejs$/")
@@ -114,6 +222,32 @@ function initializeazaErori(){  // citeste fisierul json
 }
 
 initializeazaErori();
+
+
+function initializeazaImagini(){  // citeste fisierul json
+    var continut = fs.readFileSync(__dirname+"/resurse/json/galerie.json").toString("utf-8");
+    obGlobal.obImagini = JSON.parse(continut);  // parsare -> identifica proprietatile si valorile din json
+    let vImagini=obGlobal.obImagini.imagini;
+    let caleAbs=path.join(__dirname, obGlobal.obImagini.cale_galerie);
+    let caleAbsMediu=path.join(__dirname,obGlobal.obImagini.cale_galerie, "mediu");  // folder in care vom crea imag de dimensiune medie
+    let caleAbsMic=path.join(__dirname,obGlobal.obImagini.cale_galerie, "mic");
+    if(!fs.existsSync(caleAbsMediu))
+        fs.mkdirSync(caleAbsMediu);
+    if(!fs.existsSync(caleAbsMic))
+        fs.mkdirSync(caleAbsMic);
+    for(let imag of vImagini){
+        [numeFis, ext]= imag.fisier.split(".");
+        let caleFisAbs=path.join(caleAbs,imag.fisier);
+        let caleFisMediuAbs = path.join(caleAbsMediu, numeFis +".webp");
+        let caleFisMicAbs=path.join(caleAbsMic,numeFis+".webp");
+        sharp(caleFisAbs).resize(400).toFile(caleFisMediuAbs);
+        sharp(caleFisAbs).resize(200).toFile(caleFisMicAbs);
+
+        imag.fisier_mediu=path.join("/", obGlobal.obImagini.cale_galerie, "mediu",numeFis+".webp" )
+        imag.fisier=path.join("/", obGlobal.obImagini.cale_galerie, imag.fisier )
+    }
+}
+initializeazaImagini();
 
 /*
 daca programatorul seteaza titlul, se ia titlul din argument
